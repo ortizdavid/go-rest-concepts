aee68248b521a7030282103f2d0bd60d678c5dcd
ortizdavid <ortizaad1994@gmail.com>
2024-03-06 16:52:02 +0100

    refactoring ApiKeyMiddleware function

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -28,6 +28,11 @@ func TokenAuthMiddleware(next http.Handler) http.Handler {
 func ApiKeyAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		apiKey := r.Header.Get("X-API-Key")
+		if apiKey == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintf(w, "Unauthoried. API Key missing")
+			return
+		}
 		if apiKey != validApiKey {
 			w.WriteHeader(http.StatusUnauthorized)
 			fmt.Fprintf(w, "Unauthorized. Invalid API Key")
